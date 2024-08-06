ba7ba6f3ab4e8b4a1b8a84304304c8acb6a97062
Jangwoo Park <p.jangwoo@gmail.com>
2024-08-07 01:47:14 +0900

    INIT-First Commit

---
+++ src/main/java/com/pjw/cicd_proj_01/CicdProj01Application.java
@@ -0,0 +1,13 @@
+package com.pjw.cicd_proj_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdProj01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjw/cicd_proj_01/CicdProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjw.cicd_proj_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProj01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
